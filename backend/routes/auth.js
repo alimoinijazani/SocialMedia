bc45a621e4275b79fc617a3b9f403684c78668c0
alimoinijazani <Ali_28848>
2023-01-13 10:19:30 +0330

    hashing password with bcrypt

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,16 +1,18 @@
 import express from 'express';
 import User from '../models/userModel.js';
-
+import bcrypt from 'bcrypt';
 const authRouter = express.Router();
 //register
 
 authRouter.post('/register', async (req, res) => {
-  const newUser = await new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  });
   try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (err) {
